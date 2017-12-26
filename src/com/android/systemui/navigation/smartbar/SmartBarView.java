848dbe729ded3e97b5c8b9bd26a208665842ae8a
ezio84 <brabus84@gmail.com>
2017-12-26 14:16:46 +0100

    Smartbar custom icon scaling option: fix white buttons on white navbar
    Signed-off-by: xyyx <xyyx@mail.ru>

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -128,6 +128,7 @@ public class SmartBarView extends BaseNavigationBar {
             } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.SMARTBAR_CUSTOM_ICON_SIZE))) {
                 updateCustomIconSize();
                 updateCurrentIcons();
+                reapplyDarkIntensity();
             } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.SMARTBAR_DOUBLETAP_SLEEP))) {
                 updateNavDoubletapSetting();
             }
@@ -452,6 +453,10 @@ public class SmartBarView extends BaseNavigationBar {
         setMenuVisibility(mShowMenu, true);
         setDisabledFlags(mDisabledFlags, true);
 
+        reapplyDarkIntensity();
+    }
+
+    private void reapplyDarkIntensity() {
         mBarTransitions.reapplyDarkIntensity();
     }
 
